65cb86753e1ad15d7db51ff6c9ba294a1be37c6a
Janic Duplessis <janicduplessis@gmail.com>
2019-12-16 12:50:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-native-incremental
+
+Vendor of https://github.com/facebook/react-native/tree/2ee11711e0351f695ec1d5e591ac904e5c9187c4/Libraries/Experimental
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const Incremental = require('./Incremental');
+const IncrementalGroup = require('./IncrementalGroup');
+const IncrementalPresenter = require('./IncrementalPresenter');
+
+module.exports = {
+  Incremental,
+  IncrementalGroup,
+  IncrementalPresenter,
+};
